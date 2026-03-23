5ab31ccf58454d25bb54a78a384defe3c411e0dc
leonardo <leonardooliveiragaron@gmail.com>
2026-03-23 20:49:49 -0300

    Primeira versão do jogo de adivinhação

---
+++ jogo-adivinhacao-console/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Olá, vamos jogar um jogo, voce vai tentar descobrir o numero que estou pensando entre 1 e 20...");
+
+Random num1 = new Random();
+int valor1 = num1.Next(1, 21);
+ 
+Console.WriteLine("tente adivinhar!");
+int resposta = Convert.ToInt32(Console.ReadLine());
+
+int tent = 1;
+
+while (resposta != valor1)
+{
+    if (resposta > valor1)
+    {
+        Console.WriteLine("O numero que estou pensando é menor do que isso! tente novamente.");
+        resposta = Convert.ToInt32(Console.ReadLine());
+        tent ++ ;
+    }
+
+    else if (resposta < valor1)
+    {
+        Console.WriteLine("O numero que estou pensando é maior do que isso! tente novamente.");
+        resposta = Convert.ToInt32(Console.ReadLine());
+        tent++;
+    }
+
+    else
+    {
+        Console.WriteLine("Numero invalido tente de novo");
+        resposta = Convert.ToInt32(Console.ReadLine());
+        tent++;
+    }
+}
+
+if  (resposta == valor1)
+{     
+    Console.WriteLine($"Parabens voce acertou o numero que eu estava pensando! e voce precisou de {tent} tentativas para isso!"); 
+}
