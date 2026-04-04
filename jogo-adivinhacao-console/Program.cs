5325742e103759911b2466787267001a0f8aca08
leonardo <leonardooliveiragaron@gmail.com>
2026-04-03 22:57:52 -0300

    Ajusta lógica de tentativas e limite do jogo

--- jogo-adivinhacao-console/Program.cs
+++ jogo-adivinhacao-console/Program.cs
@@ -1,35 +1,40 @@
-﻿Console.WriteLine("Olá, vamos jogar um jogo, voce vai tentar descobrir o numero que estou pensando entre 1 e 20...");
+﻿Console.WriteLine("Olá, vamos jogar um jogo, voce vai tentar descobrir o numero que estou pensando entre 1 e 20 com apenas 5 tentativas...");
 
 Random num1 = new Random();
 int valor1 = num1.Next(1, 21);
  
-Console.WriteLine("tente adivinhar!");
+Console.WriteLine("Tentativa 1 de 5, tente adivinhar!");
 int resposta = Convert.ToInt32(Console.ReadLine());
 
-int tent = 1;
+int tent = +1;
 
 while (resposta != valor1)
 {
+
+    
+
     if (resposta > valor1)
     {
-        Console.WriteLine("O numero que estou pensando é menor do que isso! tente novamente.");
-        resposta = Convert.ToInt32(Console.ReadLine());
-        tent ++ ;
+        Console.WriteLine($"tentativa {tent} de 5, O numero que estou pensando é menor do que isso! tente novamente.");
+
     }
 
     else if (resposta < valor1)
     {
-        Console.WriteLine("O numero que estou pensando é maior do que isso! tente novamente.");
-        resposta = Convert.ToInt32(Console.ReadLine());
-        tent++;
+        Console.WriteLine($"tentativa {tent} de 5, O numero que estou pensando é maior do que isso! tente novamente.");
+      
     }
 
-    else
+
+    if (tent == 5)
     {
-        Console.WriteLine("Numero invalido tente de novo");
-        resposta = Convert.ToInt32(Console.ReadLine());
-        tent++;
+        Console.WriteLine($"ultima tentativa, Infelizmente voce perdeu, o numero que eu estava pensando era {valor1}");
+        break;
     }
+
+    resposta = Convert.ToInt32(Console.ReadLine());
+    tent++;
+
 }
 
 if  (resposta == valor1)
